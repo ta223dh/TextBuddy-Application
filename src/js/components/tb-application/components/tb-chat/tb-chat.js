f722538bc0c4ea60046ac9014bd54a4a42646a7f
Tobias Aihonen <ta223dh@student.lnu.se>
2023-11-29 23:02:29 +0100

    Update to textbuddy 0.1.10. Update UI. Support chat functionality with OpenAi Api integration

--- src/js/components/tb-application/components/tb-chat/tb-chat.js
+++ src/js/components/tb-application/components/tb-chat/tb-chat.js
@@ -4,42 +4,89 @@ const template = document.createElement('template')
 template.innerHTML = `
   <style>
     #container {
-      background-color: gray;
-      width: 300px;
-      height: 50px;
+      background-color: lightpink;
       box-sizing: border-box;
       overflow: hidden;
-      color: white;
+      color: black;
       text-align: center;
       border-radius: 5px;
-      position:fixed;
-      bottom: 20px;
-      right: 20px;
-      border: 1px solid gray;
+      margin: 20px;
     }
-    #container:hover {
-      cursor: pointer;
-      border: 1px solid lightgray;
+
+    #container {
+      box-sizing: border-box;
+      width: 100%;
+      display: flex;
+  flex-direction: column;
+
     }
-    #container:active {
-      cursor: pointer;
-      border: 1px solid black;
-    }
+  
     h1 {
       font-size: 20px;
     }
 
-    tb-onboarding {
-      display: none;
-    }
-  </style>
-  <div id="container">
-  <tb-onboarding>
-  </tb-onboarding>
+    #messages {
+      overflow-y: auto; 
+      padding: 20px;
 
-    <h1 id="button">
-      Start Ai-Chat
-    </h1>
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+
+
+    }
+
+    #question, #answer {
+      padding-bottom: 20px;
+
+    }
+
+    textarea {
+      border-radius: 5px;
+      font-size: 1 rem;
+      border: none;
+      resize: none;
+      outline: none;
+      padding: 10px;
+      margin: 10px;
+      box-sizing: border-box;
+      height: 50px;
+      flex: 1;
+
+    }
+
+  #input {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+}
+
+    button {
+      border-radius: 5px;
+
+      padding: 10px;
+      margin: 10px;
+
+      box-sizing: border-box;
+      height: 50px;
+    }
+
+    button:hover {
+      cursor: pointer;
+    }
+
+
+  </style>
+
+  <div id="container">
+  <div id="messages">
+  <div id="question">Question:</div>
+  <div id="answer">Answer:</div>
+  </div>
+  <div id="input">
+  <textarea placeholder="Enter OpenAi API key..."></textarea><button>Send</button>
+  </div>
   </div>
 `
 
@@ -48,9 +95,11 @@ customElements.define('tb-chat',
    * tb-chat.
    */
   class extends HTMLElement {
-    #container
-    #startAiChat
+    #textarea
+    #button
     #ApiKey
+    #question
+    #answer
 
     /**
      * Create an instance of tb-chat.
@@ -60,17 +109,47 @@ customElements.define('tb-chat',
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
 
-        this.#container = this.shadowRoot.querySelector('#container')
-        this.#startAiChat = this.shadowRoot.querySelector('#button')
+        this.#textarea = this.shadowRoot.querySelector('textarea')
+        this.#button = this.shadowRoot.querySelector('button')
+        this.#question = this.shadowRoot.querySelector('#question')
+        this.#answer = this.shadowRoot.querySelector('#answer')
 
-        this.#startAiChat.addEventListener('click', (event) => this.startAiChat(event))  
+        this.#button.addEventListener('click', (event) => this.startAiChat(event))  
+    }
+
+    static get observedAttributes() {
+      return ['answer', 'error'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (name === 'answer' && newValue !== oldValue) {
+        this.#answer.textContent = 'Answer: ' + newValue;
+      }
+      else if (name === 'error') {
+        this.#answer.textContent = 'Answer: ' + newValue;
+      }
     }
 
     startAiChat (event) {
       if (this.#ApiKey == null) {
-        this.shadowRoot.querySelector('tb-onboarding').style.display = 'block';
-        this.shadowRoot.querySelector('#button').style.display = 'none';
-
+        this.#ApiKey = this.#textarea.value
+        const customEvent = new CustomEvent('apiKey', {
+          detail: this.#ApiKey,
+          bubbles: true,
+        });
+        this.dispatchEvent(customEvent);
+        console.log('SEND!!')
+        this.#textarea.placeholder = 'Enter a question...'
+        this.#textarea.value = '';
+      } else {
+        const customEvent = new CustomEvent('question', {
+          detail: this.#textarea.value,
+          bubbles: true,
+        });
+        this.#question.textContent = 'Question: ' + this.#textarea.value
+        this.dispatchEvent(customEvent);
+        this.#textarea.value = '';
+        this.#answer.textContent = 'Answer: '
       }
     }
   }
--- src/js/components/tb-application/tb-application.js
+++ src/js/components/tb-application/tb-application.js
@@ -1,25 +1,24 @@
 import './components/tb-chat'
+
 import TextBuddy from 'textbuddy'
 
 const template = document.createElement('template')
 template.innerHTML = `
   <style>
+
     #app {
-      background-color: pink;
-      width: 100%;
       height: 100%;
-      box-sizing: border-box;
-      overflow: hidden;
-      z-index: 1;
-    }
-    #container {
       width: 100%;
-      height: calc(100% - 115px);
+      padding: 20px;
+    box-sizing: border-box;
+    }
+
+    tb-chat {
       box-sizing: border-box;
       display: flex;
-      flex-direction: row;
+      height: 100%;
     }
-    textarea {
+    #text {
       border-radius: 5px;
       font-size: 1.2rem;
       border: none;
@@ -28,36 +27,77 @@ template.innerHTML = `
       padding: 20px;
       margin: 20px;
       box-sizing: border-box;
-
+      height: 100%;
     }
-    #container.div, textarea, h1 {
+
+    #content {
+      height: 100%;
       width: 100%;
-      box-sizing: border-box;
-    }
-    #container div {
-      margin: 0px 20px;
-      box-sizing: border-box;
-      overflow: hidden;
-      width: 500px;
 
+      box-sizing: border-box;
+
+      display: flex;
+      flex-direction: row;
     }
+
+    main {
+      box-sizing: border-box;
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      width: 100%;
+      flex: 1;
+    }
+
+    aside {
+      height: 100%;
+      max-width: 50%;
+      box-sizing: border-box;
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+    }
+    
     h1 {
+      box-sizing: border-box;
+
       height: 40px;
       text-align: center;
       color: black;
       text-shadow: white 2px 2px;
       margin-top: 50px;
     }
+
+    #data-display p {
+      margin: 0px;
+      padding: 0px;
+      line-height: 20px;
+    }
+    #data-display  {
+      display: flex;
+      box-sizing: border-box;
+      padding: 20px;
+      margin: 20px;
+      background-color: lightpink;
+      color: black;
+      border-radius: 5px;
+
+    }
   </style>
   <div id=app>
-      <h1>TextBuddy - Test Auto Deploy 2</h1>
-  <div id="container">
-    <textarea></textarea>
-    <div></div>
-    <tb-chat></tb-chat>
+    <div id="content">
+      <main>
+      <header>
+      <h1>TextBuddy</h1>
+    </header>
 
-  </div>
-
+        <textarea id="text"></textarea>
+      </main>
+      <aside>
+        <div id="data-display"><div>hej</div></div>
+        <tb-chat></tb-chat>
+      <aside>
+  </main>
   </div>
 `
 
@@ -67,7 +107,11 @@ customElements.define('tb-application',
    */
   class extends HTMLElement {
     #textArea
-    #container
+    #dataDisplay
+    #aside
+    #apiKey
+    #textBuddy
+    #tbchat
 
     /**
      * Create an instance of TextBuddy application.
@@ -78,10 +122,34 @@ customElements.define('tb-application',
         .appendChild(template.content.cloneNode(true))
 
       this.#textArea = this.shadowRoot.querySelector('textarea')
-      this.#container = this.shadowRoot.querySelector('#container')
+      this.#dataDisplay = this.shadowRoot.querySelector('#data-display')
+      this.#tbchat = this.shadowRoot.querySelector('tb-chat')
+
 
       this.#textArea.addEventListener('keydown', (event) => this.#analyse(event))
       this.#textArea.addEventListener('keyup', (event) => this.#analyse(event))
+
+
+      this.#aside = this.shadowRoot.querySelector('aside')
+
+      this.#aside.addEventListener('apiKey', (event) => {
+        this.#apiKey = event.detail
+        console.log(`Received event: ${event.detail}`);
+        this.#textBuddy.setOpenAiApiKey(event.detail)
+      });
+
+      this.#aside.addEventListener('question', async (event) => {
+
+        try {
+          let answer = await this.#textBuddy.aiAnswerQuestion(event.detail);
+          this.#tbchat.setAttribute('answer', answer);
+        } catch (error) {
+          console.log(error)
+          this.#tbchat.setAttribute('error', error.message);
+
+        }
+        console.log(`Received event: ${event.detail}`);
+      });
 
       this.#analyse()
     }
@@ -91,10 +159,14 @@ customElements.define('tb-application',
     }
 
     #analyse (event) {
-      const text = new TextBuddy(this.#textArea.value)
-      const result = text.fullAnalyzis()
+      this.#textBuddy = new TextBuddy(this.#textArea.value)
+      if (this.#apiKey !== null) {
+        this.#textBuddy.setOpenAiApiKey(this.#apiKey)
+      }
+      const result = this.#textBuddy.fullAnalyzis()
 
-      let displayArea = this.shadowRoot.querySelector('#container div')
+      let displayArea = this.#dataDisplay.querySelector('div')
+      displayArea.style.background = "red"
       let div = document.createElement('div')
 
       for (const [key, value] of Object.entries(result)) {
@@ -120,7 +192,7 @@ customElements.define('tb-application',
           div.appendChild(p)
         }
       }
-      this.#container.replaceChild(div, displayArea)
+      this.#dataDisplay.replaceChild(div, displayArea)
     }
   }
 )
